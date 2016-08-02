fb6d79457b639237018013ae85f4561b9d479329
Joren Saey <joren.saey@gmail.com>
2016-08-02 14:14:42 +0200

    Connection string

--- PackingListApp/TravelListService/Models/TravelListServiceContext.cs
+++ PackingListApp/TravelListService/Models/TravelListServiceContext.cs
@@ -16,7 +16,7 @@ namespace TravelListServiceService.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
 
-        private const string connectionStringName = "Name=MS_TableConnectionString";
+        private const string connectionStringName = "Name=MS_TravelList";
 
         public TravelListServiceContext() : base(connectionStringName)
         {
