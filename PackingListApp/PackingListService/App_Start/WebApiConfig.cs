14a65d3bf9bf3999121e9c11d4b56df3b08f1fc8
Joren Saey <joren.saey.t2795@student.hogent.be>
2016-08-12 17:03:45 +0200

    Jeeej :) Inner data van User wordt gereturned (Travels) De view in de app toont deze data

--- PackingListApp/PackingListService/App_Start/WebApiConfig.cs
+++ PackingListApp/PackingListService/App_Start/WebApiConfig.cs
@@ -8,9 +8,53 @@ using PackingListService.Models;
 using Microsoft.WindowsAzure.Mobile.Service;
 using System.Data.Entity.Validation;
 using System.Diagnostics;
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using System.Net.Http;
+using System.Linq;
 
 namespace PackingListService
 {
+    public class QueryableExpandAttribute : ActionFilterAttribute
+    {
+        private const string ODataExpandOption = "$expand=";
+
+        public QueryableExpandAttribute(string expand)
+        {
+            this.AlwaysExpand = expand;
+        }
+
+        public string AlwaysExpand { get; set; }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            HttpRequestMessage request = actionContext.Request;
+            string query = request.RequestUri.Query.Substring(1);
+            var parts = query.Split('&').ToList();
+            bool foundExpand = false;
+            for (int i = 0; i < parts.Count; i++)
+            {
+                string segment = parts[i];
+                if (segment.StartsWith(ODataExpandOption, StringComparison.Ordinal))
+                {
+                    foundExpand = true;
+                    parts[i] += "," + this.AlwaysExpand;
+                    break;
+                }
+            }
+
+            if (!foundExpand)
+            {
+                parts.Add(ODataExpandOption + this.AlwaysExpand);
+            }
+
+            UriBuilder modifiedRequestUri = new UriBuilder(request.RequestUri);
+            modifiedRequestUri.Query = string.Join("&",
+                                        parts.Where(p => p.Length > 0));
+            request.RequestUri = modifiedRequestUri.Uri;
+            base.OnActionExecuting(actionContext);
+        }
+    }
     public static class WebApiConfig
     {
         public static void Register()
