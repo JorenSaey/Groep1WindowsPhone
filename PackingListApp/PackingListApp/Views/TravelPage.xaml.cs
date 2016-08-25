d841093497e9f0b157b3d3f02619ff9d815ac115
Joren Saey <joren.saey.t2795@student.hogent.be>
2016-08-25 13:44:24 +0200

    TravelPage opgeschoont met databinding,
    Refreshen werkt
    TODO:
    Delete

--- PackingListApp/PackingListApp/Views/TravelPage.xaml.cs
+++ PackingListApp/PackingListApp/Views/TravelPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Windows.Media.Imaging;
 using System.Windows.Controls.Primitives;
 using PackingListApp.Views.PopUps;
+using System.Collections.Generic;
 
 namespace PackingListApp.Views
 {
@@ -17,6 +18,7 @@ namespace PackingListApp.Views
     {
         private UserRepository userRepo;
         private User activeUser;
+        private IList<Travel> travels;
 
         public TravelPage()
         {
@@ -25,7 +27,7 @@ namespace PackingListApp.Views
         }
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-             RefreshTravels();
+             InitTravels();
         }
         private void BtnGoToItemPage_Click(object sender, RoutedEventArgs e)
         {
@@ -60,69 +62,32 @@ namespace PackingListApp.Views
             {
                 Grid grid = (Grid)item.Content;
                 //TODO
+
             }
             RefreshTravels();
         }
-        private async void RefreshTravels()
+        private async void InitTravels()
         {
-            TravelContainer.Items.Clear();
             try
             {
                 string email = NavigationContext.QueryString["email"];
                 activeUser = await userRepo.Find(email);
-                foreach (Travel t in activeUser.Travels)
-                {
-                    //1 ListBoxItem
-                    ListBoxItem item = new ListBoxItem();
-                    //Properties of ListBoxItem
-                    item.BorderThickness = new Thickness(0, 0, 0, 1);
-                    item.BorderBrush = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.White);
-                    item.Padding = new Thickness(0, 10, 0, 10);
-                    item.IsSelected = false;
-                    item.Hold += ListBoxItem_Hold;
-                    //Content of ListBoxItem
-                    Grid grid = new Grid();
-                    ColumnDefinition col1 = new ColumnDefinition();
-                    col1.Width = new GridLength(280);
-                    ColumnDefinition col2 = new ColumnDefinition();
-                    col2.Width = new GridLength(50);
-                    ColumnDefinition col3 = new ColumnDefinition();
-                    col3.Width = GridLength.Auto;
-                    grid.ColumnDefinitions.Add(col1);
-                    grid.ColumnDefinitions.Add(col2);
-                    grid.ColumnDefinitions.Add(col3);
-                    TextBlock name = new TextBlock();
-                    name.Text = t.Name;
-                    name.FontSize = 24;
-                    name.VerticalAlignment = VerticalAlignment.Center;
-                    TextBlock percentage = new TextBlock();
-                    percentage.Text = "0%";
-                    percentage.FontSize = 24;
-                    percentage.HorizontalAlignment = HorizontalAlignment.Left;
-                    percentage.VerticalAlignment = VerticalAlignment.Center;
-                    Button button = new Button();
-                    BitmapImage bitMapImage = new BitmapImage(new Uri("/Assets/Images/list.png", UriKind.Relative));
-                    bitMapImage.DecodePixelWidth = 200;
-                    Image image = new Image();
-                    image.Source = bitMapImage;
-                    image.Width = 50;
-                    button.Content = image;
-                    button.BorderThickness = new Thickness(0, 0, 0, 0);
-                    button.Click += BtnGoToItemPage_Click;
-                    grid.Children.Add(name);
-                    Grid.SetRow(name, 0);
-                    Grid.SetColumn(name, 0);
-                    grid.Children.Add(percentage);
-                    Grid.SetRow(percentage, 0);
-                    Grid.SetColumn(percentage, 1);
-                    grid.Children.Add(button);
-                    Grid.SetRow(button, 0);
-                    Grid.SetColumn(button, 2);
-                    item.Content = grid;
-                    TravelContainer.Items.Add(item);
-                }
+                travels = activeUser.Travels;
+                TravelContainer.DataContext = travels;
             }
             catch (MobileServiceInvalidOperationException ex)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+        }
+        private async void RefreshTravels()
+        {
+            try {
+                activeUser = await userRepo.Find(activeUser.Id);
+                travels = activeUser.Travels;
+                TravelContainer.DataContext = travels;
+            }
+            catch(MobileServiceInvalidOperationException ex)
             {
                 NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
             }
